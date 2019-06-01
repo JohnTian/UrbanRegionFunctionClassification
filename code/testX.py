cd3a6e28680a3cbd9ff822bbbf992db64b6e50ee
tianzhaixing <tianzhaixing@epbox.cn>
2019-06-01 15:04:10 +0800

    添加X版本的训练和测试脚本
    Signed-off-by: tianzhaixing <tianzhaixing@epbox.cn>

---
+++ code/testX.py
@@ -0,0 +1,71 @@
+# -*- encoding:utf-8 -*-
+import os
+import cv2
+import random
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from model import MultiModal
+
+
+# 选择文件夹
+dirId = input("dir id: ")
+dirId = str(dirId)
+
+# 加载训练好的模型
+graph = tf.Graph()
+sess = tf.InteractiveSession(graph=graph)
+with sess.graph.as_default():
+    with sess.as_default():
+        model = MultiModal()
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.global_variables_initializer())
+        var_list = [var for var in tf.global_variables() if "moving" in var.name]
+        var_list += [var for var in tf.global_variables() if "global_step" in var.name]
+        var_list += tf.trainable_variables()
+        saver = tf.train.Saver(var_list=var_list, max_to_keep=1)
+        last_file = tf.train.latest_checkpoint("../model/"+dirId)
+        if last_file:
+            tf.logging.info('Restoring model from {}'.format(last_file))
+            saver.restore(sess, last_file)
+
+# 载入所有测试数据
+images = []
+visits = []
+def random_crop_and_normal(image_path, h=88, w=88):
+    im = cv2.imread(image_path)
+    height, width, channel = im.shape
+    y = random.randint(1, height - h)
+    x = random.randint(1, width - w)
+    crop = im[y:y+h, x:x+w] / 255.0
+    return crop
+
+for i in range(10000):
+    image_path = "../data/test_image/test/"+str(i).zfill(6)+".jpg"
+    image = random_crop_and_normal(image_path)
+    images.append(image)
+
+    visit_path = "../data/npy/test_visit/"+str(i).zfill(6)+".npy"
+    visit = np.load(visit_path)
+    visit = np.transpose(visit, [2, 1, 0]) # 7,26,24 -> 24,26,7
+    visit = visit / np.max(visit)
+    visits.append(visit)
+
+# 每次测试1000条数据，如果显存不够可以改小一些
+predictions = []
+for i in range(10):
+    predictions.extend(sess.run(tf.argmax(model.prediction, 1),
+                          feed_dict={model.image: images[i*1000:i*1000+1000],
+                                     model.visit: visits[i*1000:i*1000+1000],
+                                     model.training: False}))
+    print(i)
+
+# 新建文件夹
+if not os.path.exists("../result/"):
+    os.mkdir("../result/")
+
+# 将预测结果写入文件
+with open("../result/result.txt", "w+") as f:
+    for index, prediction in enumerate(predictions):
+        f.write("%s \t %03d\n"%(str(index).zfill(6), prediction+1))
+print("测试完成")
---
+++ code/trainX.py
@@ -0,0 +1,206 @@
+# -*- encoding:utf-8 -*-
+import os
+import time
+import tensorflow as tf
+from tensorflow.python import keras as keras
+
+
+def read_and_decode_train(filename):
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'data': tf.FixedLenFeature([], tf.string),
+                                           'visit': tf.FixedLenFeature([], tf.string),
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                       })
+    image = tf.decode_raw(features['data'], tf.uint8)
+    image = tf.reshape(image, [100, 100, 3])
+    image = tf.random_crop(image, [88, 88, 3])
+    image = tf.cast(image, tf.float32) / 255.0
+
+    visit = tf.decode_raw(features['visit'], tf.float64)
+    visit = tf.reshape(visit, [7, 26, 24])
+    visit = tf.transpose(visit, [2, 1, 0])
+    visit = visit / tf.reduce_max(visit)
+    label = tf.cast(features['label'], tf.int64)
+    return image, visit, label
+
+def read_and_decode_valid(filename):
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'data': tf.FixedLenFeature([], tf.string),
+                                           'visit': tf.FixedLenFeature([], tf.string),
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                       })
+    image = tf.decode_raw(features['data'], tf.uint8)
+    image = tf.reshape(image, [100, 100, 3])
+    image = tf.random_crop(image, [88, 88, 3])
+    image = tf.cast(image, tf.float32)/255.0
+
+    visit = tf.decode_raw(features['visit'], tf.float64)
+    visit = tf.reshape(visit, [7, 26, 24])
+    visit = tf.transpose(visit, [2, 1, 0])
+    visit = visit / tf.reduce_max(visit)
+    label = tf.cast(features['label'], tf.int64)
+    return image, visit, label
+
+def load_training_set():
+    with tf.name_scope('input_train'):
+        image_train, visit_train, label_train = read_and_decode_train("../data/tfrecord/train.tfrecord")
+        image_batch_train, visit_batch_train, label_batch_train = tf.train.shuffle_batch(
+            [image_train, visit_train, label_train], batch_size=batch_size, capacity=2048, min_after_dequeue=2000, num_threads=4
+        )
+    return image_batch_train, visit_batch_train, label_batch_train
+
+def load_valid_set():
+    with tf.name_scope('input_valid'):
+        image_valid, visit_valid, label_valid = read_and_decode_valid("../data/tfrecord/valid.tfrecord")
+        image_batch_valid, visit_batch_valid, label_batch_valid = tf.train.shuffle_batch(
+            [image_valid, visit_valid, label_valid], batch_size=batch_size, capacity=2048, min_after_dequeue=2000, num_threads=4
+        )
+    return image_batch_valid, visit_batch_valid, label_batch_valid
+
+def build_image_model(height, width, channel):
+    input_shape = (height, width, channel)
+    image_base_model = keras.applications.Xception(
+        include_top=False,
+        weights='imagenet',
+        input_shape=input_shape)
+    image_base_model.trainable = False
+    image_x = image_base_model.output
+    image_x = keras.layers.GlobalAveragePooling2D()(image_x)
+    image_x = keras.layers.Dense(1024, activation='relu')(image_x)
+    image_y = keras.layers.Dense(nb_classes, activation='relu')(image_x)
+    image_model = keras.models.Model(inputs=image_base_model.input, outputs=image_y, name='image')
+    return image_model
+
+def build_visit_model(height, width, depth, filters=(16, 32, 64)):
+    # initialize the input shape and channel dimension, assuming
+    # TensorFlow/channels-last ordering
+    inputShape = (height, width, depth)
+    chanDim = -1
+
+    # define the model input
+    inputs = keras.layers.Input(shape=inputShape)
+
+    # loop over the number of filters
+    for (i, f) in enumerate(filters):
+        # if this is the first CONV layer then set the input
+        # appropriately
+        if i == 0:
+            x = inputs
+
+        # CONV => RELU => BN => POOL
+        x = keras.layers.Conv2D(f, (3, 3), padding="same")(x)
+        x = keras.layers.Activation("relu")(x)
+        x = keras.layers.BatchNormalization(axis=chanDim)(x)
+        x = keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+    # flatten the volume, then FC => RELU => BN => DROPOUT
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(16)(x)
+    x = keras.layers.Activation("relu")(x)
+    x = keras.layers.BatchNormalization(axis=chanDim)(x)
+    x = keras.layers.Dropout(0.5)(x)
+
+    # apply another FC layer, this one to match the number of nodes
+    # coming out of the MLP
+    x = keras.layers.Dense(9)(x)
+    x = keras.layers.Activation("relu")(x)
+
+    # construct the CNN
+    visit_model = keras.models.Model(inputs, x)
+    return visit_model
+
+
+
+def lr_schedule(epoch):
+    """Learning Rate Schedule
+
+    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    Called automatically every epoch as part of callbacks during training.
+
+    # Arguments
+        epoch (int): The number of epochs
+
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+def train(nb_classes):
+    ## Load data
+    image_batch_train, visit_batch_train, label_batch_train = load_training_set()
+    image_batch_valid, visia_batch_valid, label_batch_valid = load_valid_set()
+
+    ## Build model
+    image_model = build_image_model(88, 88, 3)
+    visit_model = build_visit_model(26, 24, 7)
+    combined_x = keras.layers.concatenate([image_model.output, visit_model.output])
+    output = keras.layers.Dense(nb_classes, activation="softmax")(combined_x)
+    model = keras.models.Model(
+        inputs=[image_model.input, visit_model.input], 
+        outputs=output, 
+        name='imageVisit'
+    )
+    model.compile(loss='categorical_crossentropy',
+                optimizer=Adam(lr=lr_schedule(0)),
+                metrics=['accuracy'])
+    model.summary()
+
+    ## Fit and Train
+    batch_size = 512
+    epochs = 200
+    num_classes = nb_classes
+    # Prepare model model saving directory.
+    save_dir = os.path.join(os.getcwd(), 'saved_models')
+    model_name = 'urbanRegion_%s_model.h5'
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    filepath = os.path.join(save_dir, model_name)
+    # Prepare callbacks for model saving and for learning rate adjustment.
+    checkpoint = keras.callbacks.ModelCheckpoint(filepath=filepath,
+                                monitor='val_acc',
+                                verbose=1,
+                                save_best_only=True)
+
+    lr_scheduler = keras.callbacks.LearningRateScheduler(lr_schedule)
+    lr_reducer = keras.callbacks.ReduceLROnPlateau(factor=np.sqrt(0.1),
+                                cooldown=0,
+                                patience=5,
+                                min_lr=0.5e-6)
+    callbacks = [checkpoint, lr_reducer, lr_scheduler]
+    # Fit
+    model.fit(
+        x=[image_batch_train, visit_batch_train],
+        y=label_batch_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=([image_batch_valid, visia_batch_valid], label_batch_valid),
+        shuffle=True,
+        callbacks=callbacks)
+
+    ## Score trained model
+    scores = model.evaluate([image_batch_valid, visia_batch_valid], label_batch_valid, verbose=1)
+    print('Test loss:', scores[0])
+    print('Test accuracy:', scores[1])
+
+
+if __name__ == "__main__":
+    nb_classes = 9
+    train(nb_classes)
