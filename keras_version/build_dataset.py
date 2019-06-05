23b31cb6107326edee28c7e072aa9716c3963a7e
tianzhaixing <tianzhaixing@126.com>
2019-06-05 17:03:23 +0800

    优化代码逻辑
    Signed-off-by: tianzhaixing <tianzhaixing@126.com>

--- keras_version/build_dataset.py
+++ keras_version/build_dataset.py
@@ -1,62 +1,16 @@
 # -*- encoding:utf-8 -*-
-# USAGE
-# python build_dataset.py
-
+# USAGE: python build_dataset.py
 # import the necessary packages
 import os
-import cv2
 import shutil
 import random
-import datetime
 import numpy as np
 import pandas as pd
 from scut import config
 from imutils import paths
+from scut.util import randomCropAndNormal
+from scut.util import visit2arrayAndNormal
 from collections import OrderedDict
-
-
-# Refer repo: https://github.com/czczup/UrbanRegionFunctionClassification.git
-# "00": 0, "01": 1, ..., "23": 23
-str2int = {}
-for i in range(24):
-	str2int[str(i).zfill(2)] = i
-
-# 访问记录内的时间从2018年10月1日起，共182天即26周, 每周7天, 将日期按日历排列
-date2position = {}
-datestr2dateint = {}
-for i in range(182):
-	date = datetime.date(day=1, month=10, year=2018)+datetime.timedelta(days=i)
-	date_int = int(date.__str__().replace("-", ""))
-	# 20181001: [0, 0]
-	date2position[date_int] = [i%7, i//7]
-	# "20181001": 20181001
-	datestr2dateint[str(date_int)] = date_int
-
-def visit2array(table):
-    strings = table[1]
-    # 7天, 26周, 每天24小时
-    init = np.zeros((7, 26, 24))
-    for string in strings:
-        temp = []
-        for item in string.split(','):
-            temp.append([item[0:8], item[9:].split("|")])
-        for date, visit_lst in temp:
-            # 第y周的第x天的到访总人数为value
-            x, y = date2position[datestr2dateint[date]]
-            for visit in visit_lst:
-                init[x][y][str2int[visit]] += 1
-    # return init / np.max(init)
-    return init
-
-
-def random_crop_and_normal(image_path, h=88, w=88):
-    im = cv2.imread(image_path)
-    height, width, _ = im.shape
-    y = random.randint(1, height - h)
-    x = random.randint(1, width - w)
-    crop = im[y:y+h, x:x+w]
-    # return crop / 255.0
-    return crop
 
 
 def build_DataSet(dataFolder):
@@ -89,20 +43,25 @@ def build_DataSet(dataFolder):
 			if not os.path.exists(dirPath):
 				os.makedirs(dirPath)
 			for filePath in filePaths:
-				## just copy
+				# ## Method 1 just copy
+				# filename = filePath.split(os.path.sep)[-1]
+				# p = os.path.sep.join([dirPath, filename])
+				# shutil.copy2(filePath, p)
+				# shutil.copy2(filePath.replace('jpg','txt'), p.replace('jpg', 'txt'))
+
+				## Method 2 copy and visit2array
 				filename = filePath.split(os.path.sep)[-1]
 				p = os.path.sep.join([dirPath, filename])
-				# copy image
 				shutil.copy2(filePath, p)
-				# copy txt
-				shutil.copy2(filePath.replace('jpg','txt'), p.replace('jpg', 'txt'))
-				# ## merge and normalize
+				visit = visit2arrayAndNormal(filePath.replace('jpg','txt'))
+				np.save(p.replace('jpg', 'npy'), visit)
+
+				# ## Method 3 merge and normalize
 				# # image
-				# image = random_crop_and_normal(filePath)
+				# image = randomCropAndNormal(filePath)
 				# image = np.reshape(image, (1, image.shape[0]*image.shape[1]*image.shape[2]))
 				# # visit
-				# table = pd.read_csv(filePath.replace('jpg','txt'), header=None, sep='\t')
-				# visit = visit2array(table)
+				# visit = visit2arrayAndNormal(filePath.replace('jpg','txt'))
 				# visit = np.reshape(visit, (1, visit.shape[0]*visit.shape[1]*visit.shape[2]))
 				# # merge 88*88*3 + 7*26*24 = 27600  96*96*3 = 27648 相差48
 				# image = np.append(image, [0]*48)
