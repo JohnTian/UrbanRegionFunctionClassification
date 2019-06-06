9d4d9031faafe0ffa0cce6c3017d86c7e55ab412
tianzhaixing <tianzhaixing@epbox.cn>
2019-06-06 10:57:45 +0800

    中间保存结果
    Signed-off-by: tianzhaixing <tianzhaixing@epbox.cn>

--- keras_version/train.py
+++ keras_version/train.py
@@ -1,146 +1,67 @@
 # -*- encoding:utf-8 -*-
-# USAGE
-# python train.py
-
-# set the matplotlib backend so figures can be saved in the background
-import matplotlib
-matplotlib.use("Agg")
-
-# import the necessary packages
+# USAGE: python train.py
 import keras
-from keras.preprocessing.image import ImageDataGenerator
-from keras.applications import Xception
-from keras.layers.core import Dropout
-from keras.layers.core import Flatten
-from keras.layers.core import Dense
-from keras.layers import Input
+import numpy as np
+from scut import config
+from scut.util import plot_training
+from scut.data_gen import create_image_gen, create_visit_gen
+from scut.models import creat_image_model, create_visit_model, lr_schedule
+
 from keras.models import Model
 from keras.optimizers import SGD
+from keras.layers.core import Dense
 from sklearn.metrics import classification_report
-from scut import config
-from imutils import paths
-import matplotlib.pyplot as plt
-import numpy as np
-import pickle
-import os
-
-def plot_training(H, N, plotPath):
-	# construct a plot that plots and saves the training history
-	plt.style.use("ggplot")
-	plt.figure()
-	plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-	plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-	plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
-	plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
-	plt.title("Training Loss and Accuracy")
-	plt.xlabel("Epoch #")
-	plt.ylabel("Loss/Accuracy")
-	plt.legend(loc="lower left")
-	plt.savefig(plotPath)
 
 
-# derive the paths to the training, validation, and testing
-# directories
-trainPath = os.path.sep.join([config.BASE_PATH, config.TRAIN])
-valPath = os.path.sep.join([config.BASE_PATH, config.VAL])
-testPath = os.path.sep.join([config.BASE_PATH, config.TEST])
+print('[INFO] loading data ...')
+trainImageAug, valImageGen, testImageGen = create_image_gen(100, 100, 3)
+trainVisitAug, valVisitGen, testVisitGen = create_visit_gen(26, 24, 7)
 
-# determine the total number of image paths in training, validation,
-# and testing directories
-totalTrain = len(list(paths.list_images(trainPath)))
-totalVal = len(list(paths.list_images(valPath)))
-totalTest = len(list(paths.list_images(testPath)))
+print('[INFO] building model ...')
+imageModel = creat_image_model(100, 100, 3)
+visitModel = create_visit_model(26, 24, 7)
+combinedInput = concatenate([imageModel.output, visitModel.output])
+x = Dense(16, activation="relu")(combinedInput)
+x = Dense(9, activation="softmax")(x)
+model = Model(inputs=[imageModel.input, visitModel.input], outputs=x)
 
-# initialize the training data augmentation object
-trainAug = ImageDataGenerator(
-	rotation_range=30,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
-
-# initialize the validation/testing data augmentation object (which
-# we'll be adding mean subtraction to)
-valAug = ImageDataGenerator()
-
-# define the ImageNet mean subtraction (in RGB order) and set the
-# the mean subtraction value for each of the data augmentation
-# objects
-mean = np.array([123.68, 116.779, 103.939], dtype="float32")
-trainAug.mean = mean
-valAug.mean = mean
-
-HEIGHT = 88
-WIDTH = 88
-CHANNEL = 3
-TARGET_SIZE = (HEIGHT, WIDTH)
-# initialize the training generator
-trainGen = trainAug.flow_from_directory(
-	trainPath,
-	class_mode="categorical",
-	target_size=TARGET_SIZE,
-	color_mode="rgb",
-	shuffle=True,
-	batch_size=config.BATCH_SIZE)
-
-# initialize the validation generator
-valGen = valAug.flow_from_directory(
-	valPath,
-	class_mode="categorical",
-	target_size=TARGET_SIZE,
-	color_mode="rgb",
-	shuffle=False,
-	batch_size=config.BATCH_SIZE)
-
-# initialize the testing generator
-testGen = valAug.flow_from_directory(
-	testPath,
-	class_mode="categorical",
-	target_size=TARGET_SIZE,
-	color_mode="rgb",
-	shuffle=False,
-	batch_size=config.BATCH_SIZE)
-
-# load the base network, ensuring the head FC layer sets are left off
-baseModel = Xception(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(HEIGHT, WIDTH, CHANNEL)))
-
-# construct the head of the model that will be placed on top of the
-# the base model
-headModel = baseModel.output
-headModel = Flatten(name="flatten")(headModel)
-headModel = Dense(128, activation="relu")(headModel)
-headModel = Dropout(0.25)(headModel)
-headModel = Dense(len(config.CLASSES), activation="softmax")(headModel)
-
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
-model = Model(inputs=baseModel.input, outputs=headModel)
-
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
-for layer in baseModel.layers:
-	layer.trainable = False
-
-# compile our model (this needs to be done after our setting our
-# layers to being non-trainable
-print("[INFO] compiling model...")
+print("[INFO] compiling model ...")
 opt = SGD(lr=1e-4, momentum=0.9)
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-# train the head of the network for a few epochs (all other layers
-# are frozen) -- this will allow the new FC layers to start to become
-# initialized with actual "learned" values versus pure random
+
+print("[INFO] config callbacks ...")
+# Prepare model model saving directory.
+save_dir = config.MODEL_PATH
+if not os.path.isdir(save_dir):
+	os.makedirs(save_dir)
+model_name = 'urbanRegion_%s_model.h5'
+filepath = os.path.join(save_dir, model_name)
+# Prepare callbacks for model saving and for learning rate adjustment.
+checkpoint = keras.callbacks.ModelCheckpoint(
+	filepath=filepath,
+	monitor='val_acc',
+	verbose=1,
+	save_best_only=True
+)
+lr_scheduler = keras.callbacks.LearningRateScheduler(lr_schedule)
+lr_reducer = keras.callbacks.ReduceLROnPlateau(
+	factor=np.sqrt(0.1),
+	cooldown=0,
+	patience=5,
+	min_lr=0.5e-6
+)
 callbacks = [
-    keras.callbacks.TensorBoard(
+	checkpoint,
+	lr_reducer,
+	lr_scheduler,
+	keras.callbacks.TensorBoard(
         log_dir='log',
         histogram_freq=0
     )
 ]
-print("[INFO] training head...")
+
+print("[INFO] training model ...")
 H = model.fit_generator(
 	trainGen,
 	steps_per_epoch=totalTrain // config.BATCH_SIZE,
@@ -149,9 +70,7 @@ H = model.fit_generator(
 	epochs=50,
 	callbacks=callbacks)
 
-# reset the testing generator and evaluate the network after
-# fine-tuning just the network head
-print("[INFO] evaluating after fine-tuning network head...")
+print("[INFO] evaluating after fine-tuning model ...")
 testGen.reset()
 predIdxs = model.predict_generator(testGen,
 	steps=(totalTest // config.BATCH_SIZE) + 1)
