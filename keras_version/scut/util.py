b792c866bfd0960d87779a559473be94fd652472
tianzhaixing <tianzhaixing@epbox.cn>
2019-06-24 18:00:01 +0800

    添加rotate
    Signed-off-by: tianzhaixing <tianzhaixing@epbox.cn>

--- keras_version/scut/util.py
+++ keras_version/scut/util.py
@@ -115,3 +115,13 @@ def computeRatioOfBlackAndWhite(imgPath):
     ratioOfBlack = 1.0 * hist[0,0] / imSize
     ratioOfWhite = 1.0 * hist[255,0] / imSize
     return (ratioOfBlack, ratioOfWhite)
+
+
+# refer: https://www.jianshu.com/p/b5c29aeaedc7
+def rotate(image, angle, center=None, scale=1.0):
+    (h, w) = image.shape[:2]
+    if center is None:
+        center = (w / 2, h / 2)
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
